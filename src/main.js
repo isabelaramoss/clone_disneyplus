9fcf8554b97f57d3dbab960477724e8ab94ba96d
Isabela Ramos <isamrteixeira@gmail.com>
2023-12-29 16:44:30 -0300

    header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
+  //header visibility
+  const heroSection = document.querySelector('.hero');
+    //pegar altura
+  const alturaHero = heroSection.clientHeight;
+
+  window.addEventListener('scroll', function(){
+                        //scroll no eixo Y (vertical)
+    const posicaoAtual = window.scrollY
+
+    if(posicaoAtual < alturaHero){
+      ocultaElementosHeader();
+    } else{
+      exibeElementosHeader();
+    }
+  })
+
+
   const buttons = document.querySelectorAll('[data-tab-button]');
   // vv abrir e fechar faq
   const questions = document.querySelectorAll('[data-faq-question]');
   
-  
+  //Seçãoa de atrações, programações das abas 
   for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', function(botao){
       const abaAlvo = botao.target.dataset.tabButton;
@@ -15,10 +32,25 @@ document.addEventListener('DOMContentLoaded', function(){
     })
   }
 
+  //Seção FAQ e accordion
   for(let i = 0; i < questions.length; i++){
     questions[i].addEventListener('click', abreOuFechaResposta);
   }
 })
+
+
+//ocultar elemento do header
+function ocultaElementosHeader(){
+  const header = document.querySelector('header');
+  header.classList.add('header--is-hidden');
+}
+
+//exibe elemento do header
+function exibeElementosHeader(){
+  const header = document.querySelector('header');
+  header.classList.remove('header--is-hidden');
+}
+
 
 function abreOuFechaResposta(elemento){
   const classe = 'faq__questions__item--is-open';
